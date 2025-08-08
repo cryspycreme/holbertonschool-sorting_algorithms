7703223fe8928e46cea9337cec98511a5534fe86
“cryspycreme” <11529@holbertonstudents.com>
2025-08-08 05:13:35 +0000

    Move print array function inside if statement

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,9 +26,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
 			j++;
-			print_array(array, size);
 		}
 		i++;
 	}
